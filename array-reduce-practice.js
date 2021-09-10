5ee152d62538e78b526af42f2736a6ba5aff2fe5
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2021-09-11 01:36:43 +0300

    Ignored some files.

--- array-reduce-practice.js
+++
@@ -1,3 +0,0 @@
-
-[1, 2, 3, 4].reduceRight((total, currentValue, currentIndex) => console.log(currentIndex));
-
--- map-practice.js
+++
@@ -1,3 +0,0 @@
-const oddNumbers = (e) => Array.from(new Array(Math.floor((e + 1) /2)), (_, i) => 2 * i + 1);
-
-console.log(oddNumbers(100));
